c24bcfb3562e2a4db31093d0ec7507f8dce283df
Samuryk Nuriddin <254452@astanait.edu.kz>
2026-01-24 11:47:26 +0500

    usage of envs

--- src/database/DatabaseConnection.java
+++ src/database/DatabaseConnection.java
@@ -6,10 +6,9 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
 
-    private static final String URL =
-            "jdbc:postgresql://localhost:5432/car_rental_db";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "postgres";
+    private static final String URL = System.getenv("DB_URL");
+    private static final String USER = System.getenv("DB_USER");
+    private static final String PASSWORD = System.getenv("DB_PASSWORD");
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
